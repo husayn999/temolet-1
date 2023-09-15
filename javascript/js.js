60e5c352e8eb451eded986cf7b008566206312ab
husayn999 <139649501+husayn999@users.noreply.github.com>
2023-09-16 01:38:28 +0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,329 @@
+
+// function test(){
+
+// // document.getElementById("h12").innerHTML = "<h1>كسمك</h1>";
+// // }
+
+// // function test2(){
+
+// // // document.getElementById("h12").innerHTML ="<h1>بحبك<h1>";
+// // // }
+// // // var mybtn = document.getElementById("btn"),
+// // //     mytxt = document.getElementById("txt"),
+// // // //     myh12 = document.getElementById("h12");
+
+// // // //    onkeypress = function calc(){
+
+// // // //           myh12.innerHTML = mytxt.value * 365 ;
+// // // // //     }
+// // // // var test = 10
+// // // // var test2 = test.toString()
+// // // // //     console.log(typeof(test2));
+// // // //  var test = "hello is world";
+// // // // //  var text2 = test.search("is");
+// // // // //      console.log(text2);
+// // // // var text = "i lovb life"
+// // // //      text2 = text.substr()
+// // // //      console.log(text2)
+// // // //      console.log(typeof(text2))
+
+// // // // var text1 = "              website is very cool";
+// // // // text2 =text1.trim();
+// // // // text3 =text1.trim();
+// // // // text4 =text1.trim();
+// // // // text5 =text1.trim();
+// // // // text6=text1.trim();
+
+// // // // console.log(text6);
+// // // // console.log(text5);
+// // // // console.log(text4);
+// // // // console.log(text3);
+// // // // // console.log(text2);
+// // // // // console.log(text1);
+// // // // // var text = "abc ";
+// // // // //     text = string.FromChrCode();
+// // // // // console.log(text2);
+// // // // var text = "i loVE"
+// // // // text2 = text.toLocaleLowerCase()
+// // // console.log(text2)
+// // // var text = "hessain said ozzy hassan";
+// // //    text2 = String.fromCharCode( 3791);
+// // // //    console.log(text2);
+// // // var text = Math.round(7.5);
+// // // console.log(text);
+
+// // // var text = Math.ceil(7.5);
+// // // console.log(text);
+
+// // // var text = Math.floor(7.5);
+// // // console.log(text);
+
+// // // var text = Math.min(7.5 , 100 , -1000 , 24);
+// // // console.log(text);
+
+// // // var text = Math.max(7.5 , 100 , -1000 , 24);
+// // // console.log(text);
+
+// // // var text = Math.random(-10);
+// // // console.log(Math.ceil(text +10));
+
+// // // let hessainknow = new Date();
+// // //   hessainknow = document.getElementById("h12");
+
+
+// // // // // console.log(hessainknow);
+// // // // const lethessainknow = new Date();
+// // // // document.getElementById("h12").innerHTML = lethessainknow;
+// // // const a =1 ;
+// // // const  a =2  ;
+// // // // console.log(a) ;
+// // // var a = 'hessa\n  "in"';
+// // // console.log(`${a}`)
+// // // console.log('hessa\n  "in"')
+// // // let par = "title"
+// // // let title = "hessain"
+
+// let a = `
+
+//  <div class="help">
+
+//     <div class="a">
+
+//        <h2>${par}<h2>
+
+//        <p>${title}<p>
+//                <div>
+//             <div>
+//         `;
+//         document.write(a)
+//         let title = "hello world", card = "cardlist", content = "elzero web", market = "for fre
+//         let a
+//         <div class= "title
+//        <h1>${title}<h
+//            <div class="content"
+//                <p>${content}<
+//            <di
+//            <div class="card"
+//                <p>${card}<
+//            <di
+//            <div class="market"
+//                <p>${market}<
+//            <di
+//        <div>
+//        document.write(a);
+
+// // console.log(1 + "osama
+//  console.log(20 * 20);
+// // // // console.log(20 ** 20
+//  console.log("hessain" + "hessain
+// console.log(1 % "hessain"
+//  console.log(1 / "hess
+//  console.log(+10
+//  console.log(+"100
+//  console.log(+"-1000
+// console.log(Number("1000"
+// let a = 20
+//  a = a +
+//  a = a + 7
+//  a +=
+// a +=
+//  a = a + 100 - 10
+//  a -= a - 1
+//  console.log(
+//      let a =
+//  let b= "20
+// let c =
+//  console.log(++a + +b++ + +c++ + a+
+//  console.log(++a + - b + +c++ - -a++ + +a
+// console.log(--c + +b + --a * + +b++ - +b * a + --a - +true);co
+
+// console.log((100.34324).toString())
+// console.log((100.34524).toFixed(2))
+
+// // console.log(parseInt("100.10"));
+// // console.log(parseFloat("100.10 he"));
+// // console.log(Number.isNaN("100"));
+// // console.log(Number.isNaN("hedsa"));
+// var text = Math.random(-10);
+// console.log(Math.ceil(text + 10));
+//  console.log(Math.trunc(99.0))
+
+// document.write(`<h1>show ${showCount} prouducts</h1>`);
+// for (let i = 0; i < showCount; i++){
+//     document.write(`<div>`)
+//     document.write(`<h2> [${i + 1}] ${prouducts[i]}</h2>`)
+//     for(let j = 0 ; j < color.length; j++) {
+//         document.write(`<p> ${color[j]} </p>`)
+//     }
+//     document.write(`<p> ${color.join( " | ")} </p>`)
+//     document.write(`</div>`)
+
+// }
+//  let prouducts = ["keyboard", "mouse", "banana", "pen", "shose", "pencile"];
+// // let color = ["green", " yellow", "black"];
+// let showCount = 0;
+// while (showCount < prouducts.length) {
+//     console.log(prouducts[showCount]);
+// //     showCount += 1;
+// // //     if (showCount === 3) {
+// // //         break;
+// // //     }
+// //  }
+// function genrateYears(start, End, exclued) {
+//     for (let i = start; i <= End; i++){
+//         if (i === exclued) {
+//             continue;
+//         }
+//         console.log(i)
+//     }
+// }
+
+// genrateYears(2004, 2023,2020);
+
+
+// function leyo (us = "unknown", ag = "unknown", rt = "unknown", show = "yes", ...sk) {
+//     document.write(`<div>`);
+//     document.write(`<h1>hello ${us}</h1>`);
+//     document.write(`<p>okay ${ag}</p>`);
+//     document.write(`<p> programoing ${sk}</p>`);
+//     if (show === "yes") {
+//         if (sk.length > 0) {
+//             document.write(`<h1>fuck: ${sk.join(" | ")}</h1>`)
+//         }
+//         document.write(`<h1>.${sk.join(" | ")}</h1>`)
+//     }
+//       else{
+//           document.write(`<h1>skills is hidden</h1>`)
+//     }
+//      document.write(`</div>`);
+//     }
+//     leyo("hessain",10 ,101 , "no")
+
+// function parent() {
+//     let a = 20;
+//     function child() {
+//         let a = 20;
+//         console.log(`from child ${a}`);
+
+//         function grandchild() {
+//             let b = 100;
+//             console.log(`from grand ${a}`);
+//             console.log(`from grand ${b}`);
+//         }
+//         grandchild();
+//     }
+//     child();
+// }
+// parent();
+// let myNums = [1, 2, 3, 4, 5, 6];
+
+// let newarray = [];
+
+// for (let i = 0; i < myNums.length; i++){
+    
+// newarray.push(myNums[i] + myNums[i]);
+// }
+// console.log(newarray);
+// // // same idea with map
+
+// // let addSelf = myNums.map((element) => element + element);
+
+// // console.log(addSelf)
+// function addition(ele) {
+//     return ele + ele;
+
+// // }
+// // let add = myNums.map(addition);
+// // console.log(add)
+// let a = "HEssAIn";
+// let num = [1, 10, 90, -100];
+
+// let aa = a
+//     .split("")
+//     .map(function (ele) {
+//     return ele === ele.toUpperCase() ? ele.toLowerCase() : ele.toUpperCase();
+//     })
+//     .join("");
+// console.log(aa);
+
+// let numbers = num.map(function (ele) {
+//     return -ele;
+
+// });
+// // console.log(numbers)
+// let friends = ["mohamed", "ahmed", "alla", "menna"];
+
+// let ff = friends.filter(function (el) {
+//     return el.startsWith("a") ? true : false;
+// });
+// console.log(ff);
+
+
+// let num = [20, 21, 123, -111, 12, 69];
+
+// let eventsfilters = num.filter(function (el) {
+//     return el % 2 === 0 ? true : false;
+// });
+// console.log(eventsfilters);
+//test map vs filter
+// let addmap = num.map(function (el) {
+//     return el + el;
+// })
+// console.log(addmap);
+// //
+// let addfilter = num.filter(function (el) {
+// //     return el + el;
+// // })
+// // console.log(addfilter);
+// let max = "A1B6CX2";
+
+// let min = max.split("").filter(function (el) {
+//     return isNaN(parseInt(el))
+// }).join("");
+// console.log(min);
+    
+// let min1 = max.split("").filter(function (el) {
+//     return !isNaN(parseInt(el));
+// }).map(function (el){
+//     return el * el;
+
+// })
+//     .join("");
+//     console.log(min1);
+
+
+
+// let alli = document.querySelectorAll("ul li");
+// let alldivs = document.querySelectorAll(".content div");
+
+// alli.forEach(function (el) {
+
+//     el.onclick = function () {
+//         alli.forEach(function (ele){
+//             ele.classList.remove("active")
+//         })
+//       this.classList.add("active")
+//         //hide all divs
+//         alldivs.forEach(function (ele) {
+//             ele.style.display = "none";
+//         })
+//     }
+    
+// });
+
+
+// let allli = document.querySelectorAll("ul li");
+// let alldivs = document.querySelectorAll(".content div");
+
+// allli.forEach(function (ele) {
+//     ele.onclick = function () {
+//         allli.forEach(function (ele) {
+//             ele.classList.remove("active");
+//         })
+//         ele.classList.add("active");
+//         alldivs.forEach(function (ele) {
+//             ele.style.display = "none";
+//         })
+
+//     };
+// })
